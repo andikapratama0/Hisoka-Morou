f4adfeba8846f6df43f08c45eef92a154fc09ed3
andikapratama0 <104635083+andikapratama0@users.noreply.github.com>
2022-07-24 23:12:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
+   * Create By DikaModz
+   * Contact Me on wa.me/6283162446604
    * Follow https://github.com/DikaArdnt
 */
 
@@ -18,8 +18,8 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6287721317870','6288292024190']
-global.premium = ['6288292024190']
+global.owner = ['6283162446604','6281382420312','6287721317870','6288292024190']
+global.premium = ['6283162446604']
 global.packname = 'Hisoka Morrow'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
